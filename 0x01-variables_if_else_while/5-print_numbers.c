5450cd9bda3297c9d2a57fc373a2f142e635644f
root <root@ROGUE-INSTRUMENT.localdomain>
2023-06-16 18:10:54 +0000

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first nine whole numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 0; z < 10; z++)
+	{
+		printf("%d", z);
+	}
+	putchar('\n');
+	return (0);
+}
