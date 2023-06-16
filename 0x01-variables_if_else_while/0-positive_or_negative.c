5b4aa530bafb98dfd47cccc598d8401ced2262df
root <root@ROGUE-INSTRUMENT.localdomain>
2023-06-16 17:13:33 +0000

    add

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Determining if a number is zero, greater than zero, or less
+ * main - Determining if a number is positive, negative or zero
  *
  * Return: Always 0 (Success)
  */
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 	
 	srand(time(0));
-	n = rand() - RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
