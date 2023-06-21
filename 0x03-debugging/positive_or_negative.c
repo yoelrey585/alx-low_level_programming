cf9551fbf6c2bd4262c5f080b04970c93086faf6
root <root@ROGUE-INSTRUMENT.localdomain>
2023-06-21 19:22:12 +0000

    add

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* positive_or_negative - determines whether number is positive or negative
+*@i: number
+* Return: void
+*/
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
