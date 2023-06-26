7588b0a6588a35043aa9e04b38b7bfbe92dc9697
root <root@ROGUE-INSTRUMENT.localdomain>
2023-06-26 17:17:27 +0000

    add

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes an int pointer as parameter
+ * updates and the value and points to 98
+ * @n: input
+ * Retun: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
