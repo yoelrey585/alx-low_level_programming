cf704b4139e086fdb3c5f05958d5a666c22a2f8a
root <root@ROGUE-INSTRUMENT.localdomain>
2023-08-01 11:35:21 +0000

    add

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a linked list.
+ * @h: Pointer to the linked list of type listint_t to print.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a linked list.
+ * @h: Pointer to the linked list of type listint_t to traverse.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
