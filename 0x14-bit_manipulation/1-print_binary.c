05af9005d2fa13b88a30fc68f92cc9564bc7d8b1
root <root@ROGUE-INSTRUMENT.localdomain>
2023-08-31 17:04:57 +0000

    add

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,14 +2,13 @@
 /**
  * print_binary - prints the binary of decimal value
  * @n: decimal number
- * 
+ *
 **/
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-    {
-        print_binary(n >> 1);
-    }
-    
-    _putchar((n & 1) + '0');
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
 }
