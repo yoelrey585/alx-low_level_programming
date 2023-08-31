752e65109fac8313f23df074f761a392f8b44bbc
root <root@ROGUE-INSTRUMENT.localdomain>
2023-08-31 16:41:15 +0000

    add

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -2,10 +2,10 @@
 /**
  * _putchar - putchar function
  * @z: character to be outputted
- * 
- * return - on success 1, on failure -1
+ *
+ * Return: on success 1, on failure -1
 **/
 int _putchar(char z)
 {
-    return (write(1, &z, 1));
+	return (write(1, &z, 1));
 }
