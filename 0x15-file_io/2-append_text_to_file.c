75a6006d5a13cc0425a6dbd5a24906d017ecfc50
root <root@ROGUE-INSTRUMENT.localdomain>
2023-09-04 13:17:23 +0000

    add

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,32 +1,39 @@
 #include "main.h"
 /**
- * append_text_to_file - appends text to end of the .txt file
- * @filename: filename pointer
- * @text_content: string of characters to be appended
+ * append_text_to_file - appends text to the end of .txt file
+ * @filename: filename
+ * @text_content: string of character to be added
  *
- * Return: 1 on success, -1 on failure and NULL filename
- * 1 if file exists and -1 if file does not exist
+ * Return: 1 on success(file exists), -1 on failure(file non-exists)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o_pen, w_rite, len = 0;
+	int fd;
+	int num_letters;
+	int rw;
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
+	if (!filename)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		for (num_letters = 0; text_content[num_letters]; num_letters++)
+			;
+
+		rw = write(fd, text_content, num_letters);
+
+		if (rw == -1)
+			return (-1);
 	}
 
-	o_pen = open(filename, O_WRONLY | O_APPEND);
-	w_rite = write(o, text_content, len);
-
-	if (o_pen == -1 || w_rite == -1)
-		return (-1);
-
-	close(o_pen);
+	close(fd);
 
 	return (1);
 }
