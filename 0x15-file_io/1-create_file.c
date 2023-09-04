b58a9ae9545b66d832478cdb75da7951430bd49c
root <root@ROGUE-INSTRUMENT.localdomain>
2023-09-04 13:04:55 +0000

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,43 @@
 #include "main.h"
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * create_file - function to create a file
+ * @filename: the name of file
+ * @text_content: string of characters to be writen
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int file_d;
+	int num_letters;
+	int rw;
 
 	if (!filename)
+	{
 		return (-1);
+	}
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
+	if (file_d == -1)
+	{
 		return (-1);
-
+	}
 	if (!text_content)
+	{
 		text_content = "";
+	}
 
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for (num_letters = 0; text_content[num_letters]; num_letters++)
 		;
 
-	rwr = write(fd, text_content, nletters);
+	rw = write(file_d, text_content, num_letters);
 
-	if (rwr == -1)
+	if (rw == -1)
+	{
 		return (-1);
+	}
 
-	close(fd);
+	close(file_d);
 
 	return (1);
 }
-
