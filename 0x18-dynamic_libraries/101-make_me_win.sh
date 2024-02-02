ec6d701490e13cf03bb0a76038eaa354cd9d0c7d
yoelrey585 <adomdarko@gmail.com>
2024-02-02 21:31:12 +0000

    add

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/dennisnderitu254/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
+wget -P /tmp/ https://github.com/yoelrey585/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD =/tmp/libtest.so
